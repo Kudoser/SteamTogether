10a8231c6b3736db5d761537ca9a1d9c2594c79b
Andrei Blizorukov <andrey.blizorukov@trivago.com>
2023-05-06 10:20:25 +0200

    +parser tests

---
+++ SteamTogether.Scraper.UnitTests/TelegramCommandParserTests.cs
@@ -0,0 +1,18 @@
+﻿using SteamTogether.Bot.Services.Command.Parser;
+
+namespace SteamTogether.Scraper.UnitTests;
+
+public class TelegramCommandParserTests
+{
+    [Theory]
+    [InlineData("/play Co-op", "play", new [] {"Co-op"})]
+    [InlineData("/list", "list", new string[]{})]
+    public void CommandAndArguments_Should_Be_ParsedTest(string input, string expectedCommand, string[] expectedArgs)
+    {
+        var sut = new TelegramCommandParser();
+        var result = sut.Parse(input);
+        
+        Assert.Equal(expectedCommand, result.CommandName);
+        Assert.Equal(expectedArgs, result.Arguments);
+    }
+}
